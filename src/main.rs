d10ead47425567dd28afbfdd0b1d320658737ccd
Pinakpani Mukherjee <mukherjee.pinakpani14@gmail.com>
2022-09-27 16:01:27 +0530

    added main struct

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let server = Server::new("127.0.0.1:8080");
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self { addr: addr }
+    }
+
+    fn run(self) {}
+}
