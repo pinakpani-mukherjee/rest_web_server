baf0fffb375ee35439fc5830cc26ad5b7da7d851
Pinakpani Mukherjee <mukherjee.pinakpani14@gmail.com>
2022-10-01 14:42:01 +0530

    added custom error handling

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,14 +1,52 @@
 use super::method::Method;
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
 }
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
+    type Error = ParseError;
 
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         unimplemented!()
     }
 }
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message())
+    }
+}
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        match self {
+            Self::InvalidRequest => write!(f, "InvalidRequest"),
+            Self::InvalidEncoding => write!(f, "InvalidEncoding"),
+            Self::InvalidProtocol => write!(f, "InvalidProtocol"),
+            Self::InvalidMethod => write!(f, "InvalidMethod"),
+        }
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+impl Error for ParseError {}
