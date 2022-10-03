63897792c86d8c9845ab218f794130e82b061a98
Pinakpani Mukherjee <mukherjee.pinakpani14@gmail.com>
2022-10-04 00:00:07 +0530

    added website handling

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
-use crate::http::{Request, Response, StatusCode};
+use crate::http::{ParseError, Request, Response, StatusCode};
 use std::convert::TryFrom;
+use std::fmt::Error;
 use std::io::{Read, Write};
 use std::net::TcpListener;
 
@@ -7,12 +8,20 @@ pub struct Server {
     addr: String,
 }
 
+pub trait Handler {
+    fn handle_request(&mut self, request: &Request) -> Response;
+    fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+        println!("Failed to Parse Request:{}", e);
+        Response::new(StatusCode::BadRequest, None)
+    }
+}
+
 impl Server {
     pub fn new(addr: String) -> Self {
         Self { addr: addr }
     }
 
-    pub fn run(self) {
+    pub fn run(self, mut handler: impl Handler) {
         println!("Listening on {}", self.addr);
         let listener = TcpListener::bind(&self.addr).unwrap();
         loop {
@@ -24,14 +33,8 @@ impl Server {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
                             let response = match Request::try_from(&buffer[..]) {
-                                Ok(request) => Response::new(
-                                    StatusCode::Ok,
-                                    Some("<h1>All I got was a lousy h1 tag???</h1>".to_string()),
-                                ),
-                                Err(e) => {
-                                    println!("Failed to parse a request: {}", e);
-                                    Response::new(StatusCode::BadRequest, None)
-                                }
+                                Ok(request) => handler.handle_request(&request),
+                                Err(e) => handler.handle_bad_request(&e),
                             };
                             if let Err(e) = response.send(&mut stream) {
                                 println!("Failed to send response: {}", e);
