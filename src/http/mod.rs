8ea345c5054cc625f7795c45eade9ab46ef4e04b
Pinakpani Mukherjee <mukherjee.pinakpani14@gmail.com>
2022-10-03 19:18:06 +0530

    added hashmap for query

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,6 +1,9 @@
 pub use method::Method;
+pub use query_string::{QueryString, Value as QueryStringValue};
 pub use request::ParseError;
 pub use request::Request;
 
 pub mod method;
 pub mod request;
+
+pub mod query_string;
